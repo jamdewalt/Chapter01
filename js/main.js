1892d478d9280c048649fbc86acca4a3b4b76b6f
jamdewalt <98490986+jamdewalt@users.noreply.github.com>
2022-02-02 14:48:31 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* Scripts by James DeWalt, 2022 */
+var mydiv = document.getElementById("mydiv");
+mydiv.addEventListener("click", function(){
+    alert("I have been clicked! Again!");
+var madison = {
+    name: "Madison",
+    county: "Dane",
+    population: 250000,
+}
+document.getElementById('mydiv').innerHTML = madison.name;
+
+var cityList = [{
+    name:'Madison'
+},
+{
+    name:'Milwaukee'
+},
+
+]
+
+
+
+});
