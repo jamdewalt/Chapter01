27bc6206926d0bb16de55e2cb8c274c20aace95b
jamdewalt <98490986+jamdewalt@users.noreply.github.com>
2022-02-02 14:22:27 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@
 var mydiv = document.getElementById("mydiv");
 mydiv.addEventListener("click", function(){
     alert("I have been clicked! Again!");
+var madison = {
+    name: "Madison",
+    county: "Dane",
+    population: 250000,
+}
+document.getElementById('mydiv').innerHTML = madison.name;
+
+var cityList = [{
+    name:'Madison'
+},
+{
+    name:'Milwaukee'
+},
+
+]
+
+
+
 });
