78fa0778a5df6e6721c4025416e1942a8ca30842
jamdewalt <98490986+jamdewalt@users.noreply.github.com>
2022-02-07 18:48:45 -0600

    Activity 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Scripts by James DeWalt, 2022 */
 var mydiv = document.getElementById("mydiv");
 mydiv.addEventListener("click", function(){
-    alert("I have been clicked! Again!");
+    alert("I have been clicked! Please Stop!");
 });
---
+++ js/main_with_debug.js
@@ -0,0 +1,130 @@
+/* Scripts by James DeWalt, 2022 */
+//this calls the cities function when the script loads
+function initialize(){
+	cities();
+};
+//this function is used to create a table with some cities and their population
+function cities(){
+    //defined two arrays for cities and population
+	var cityPop = [
+		{ 
+			city: 'Madison',
+			population: 233209
+		},
+		{
+			city: 'Milwaukee',
+			population: 594833
+		},
+		{
+			city: 'Green Bay',
+			population: 104057
+		},
+		{
+			city: 'Superior',
+			population: 27244
+		}
+	];
+	//this creates a table element
+	var table = document.createElement("table");
+
+	//this creates a header row element and appends it to the table
+	var headerRow = document.createElement("tr");
+	table.appendChild(headerRow);
+
+	//this creates the "City" and "Population" column headers
+	headerRow.insertAdjacentHTML("beforeend","<th>City</th><th>Population</th>")
+	
+	//this uses a loop to add a new row for each city
+cityPop.forEach(function(cityObject){
+		
+		// this assigns longer html strings to a variable
+		var rowHtml = "<tr><td>" + cityObject.city + "</td><td>" + cityObject.population + "</td></tr>";
+		//this adds the row's html string to the table
+		table.insertAdjacentHTML('beforeend',rowHtml);
+	})
+	
+	//this appends the table element to the div
+	document.querySelector("#mydiv").appendChild(table);
+
+    addColumns(cityPop);
+    addEvents();
+
+};
+
+//this function is used to add the city size column to the table
+function addColumns(cityPop){
+    
+	// this selects all the column rows
+	var rows = document.querySelectorAll("tr")
+	
+	// this loop adds a column for each row
+	document.querySelectorAll("tr").forEach(function(row, i){
+
+    	if (i == 0){
+
+    		row.insertAdjacentHTML('beforeend', '<th>City Size</th>'); // fixed incorrect spelling of adjacent
+    	} else {
+			// this determines the size based on total population there
+    		var citySize;
+
+    		if (cityPop[i-1].population < 100000){
+    			citySize = 'Small';
+
+    		} else if (cityPop[i-1].population < 500000){
+    			citySize = 'Medium';  // fixed misspelled variable name citysize
+
+    		} else {
+    			citySize = 'Large';
+    		};
+				//this adds a new table cell with the city size
+				var newRow = document.createElement('td')
+				newRow.innerHTML = citySize
+				//this appends the city size cell to table
+				row.appendChild(newRow)	
+			row.insertAdjacentHTML = '<td' + citySize + '</td>'; // fixed incorrect spelling of adjacent
+    	};
+    });
+};
+
+// this function adds mouse events. Fun Fun!
+function addEvents(){
+
+	// This selects the table element
+	table = document.querySelector("table");
+	
+	// adds a mouseover event for the table
+	document.querySelector("table").addEventListener("mouseover", function(){
+		
+		var color = "rgb(";
+
+		for (var i=0; i<3; i++){
+
+			var random = Math.round(Math.random() * 255);
+
+			color += random;
+
+			if (i<2){
+				color += ",";
+			
+			} else {
+				color += ")";
+		};
+
+	}
+		//this gives the style table a random color. Creates rainbow when you hover over the tables
+		table.style.color = color;
+
+
+		
+	}); // added additional bracket to close off function properly
+
+	//this function is used to display text if you click on a table element
+	function clickme(){
+
+		alert('Hey, you clicked me!');
+	};
+
+};
+
+//this calls the initialize function 
+document.addEventListener('DOMContentLoaded',initialize)
